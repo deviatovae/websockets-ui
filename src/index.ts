ac486fe8e4d6479af4b71187fc611f53035997af
Alena Deviatova <aedeviatova@yandex.ru>
2023-07-06 17:38:09 +0300

    migrate from js to ts, fix run commands

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { httpServer } from "./src/http_server/index.js";
+import { httpServer as front } from './server/front';
 
 const HTTP_PORT = 8181;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
-httpServer.listen(HTTP_PORT);
+front.listen(HTTP_PORT);
